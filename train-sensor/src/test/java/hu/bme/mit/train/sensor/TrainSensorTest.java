914009e9857e87b70f7ed609e7808673542ef272
Qnbie <alszenful@gmail.com>
2020-03-02 16:16:47 +0100

    Junite3

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,9 +3,6 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import hu.bme.mit.train.controller.TrainControllerImpl;
-import hu.bme.mit.train.sensor.TrainSensorImpl;
-import hu.bme.mit.train.user.TrainUserImpl;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
